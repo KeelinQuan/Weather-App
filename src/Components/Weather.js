26386ffde01c869deca95afd0629e6d5ea67b124
keelinQuan <phamanhquan15012000@gmail.com>
2023-07-01 14:51:01 +0700

    ten bien

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -18,7 +18,6 @@ function Weather() {
     } else {
       const data = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&APPID=${APIKEY}`
-        // `https://api.openweathermap.org/data/2.5/weather?q=${form.city},&APPID=${APIKEY}`
       )
         .then((res) => res.json())
         .then((data) => data);
@@ -34,9 +33,9 @@ function Weather() {
     if (name === "city") {
       setForm({ ...form, city: value });
     }
-    if (name === "country") {
-      setForm({ ...form, country: value });
-    }
+    // if (name === "country") {
+    //   setForm({ ...form, country: value });
+    // }
   };
   return (
     <div className="weather">
@@ -50,12 +49,12 @@ function Weather() {
           onChange={(e) => handleChange(e)}
         />
         {/* &nbsp; &nbsp; &nbsp;&nbsp; */}
-        <input
+        {/* <input
           type="text"
           placeholder="country"
           name="country"
           onChange={(e) => handleChange(e)}
-        />
+        /> */}
         <button className="getweather" onClick={(e) => weatherData(e)}>
           Submit
         </button>
